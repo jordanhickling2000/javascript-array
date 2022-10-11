1b014e8e04083e37e88c3a4860e7529f48839113
Jordan Hickling <jordan.hickling@netmatters-scs.com>
2022-10-11 16:13:54 +0100

    Added more desing to font and form. Added regex code and JavaScript Updated

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const url = 'https://picsum.photos/300/300';
 const changeImage = document.querySelector('.images');
 let img = new Image(300, 300); //creates a new image at the size of 300x300
-changeImage.appendChild(img); //This should append the .images div to the new image that is created.
+changeImage.appendChild(img); //This should append the .images div to the new image that is created. The images class is the parent and its appending the child to the parent. 
 const photographer = document.querySelector('.photographer');
 const pictureBtn = document.querySelector('#picture-btn');
 
@@ -14,7 +14,9 @@ const pictureBtn = document.querySelector('#picture-btn');
   .then(resolve => {
     fetch(resolve)
     .then(response => response.json())
-  //   //changes the SRC URL of the img. (declared as a new image using a variable.)
+    // gets the data of the photographer
+    .then( data => photographer.insertAdjacentHTML('beforeend', data.author))
+    //changes the SRC URL of the img. (declared as a new image using a variable.)
     .then(img.src = url)
     // console.log(resolve);
   })
@@ -23,6 +25,7 @@ const pictureBtn = document.querySelector('#picture-btn');
 pictureBtn.addEventListener("click", () => {
   // Every click makes the SRC blank so that the URL can be placed through the new promise.
   img.src = ''
+  photographer.innerHTML = 'Photographer: '; // Resets the photographer div everytime a click is heard. Inserting the starting text "Photographer"
   
   let newLink = new Promise((resolve, reject) => {
     fetch(url)
@@ -34,41 +37,42 @@ pictureBtn.addEventListener("click", () => {
             .then( data => {
               console.log(data); //This displays the data in an array showing that the data is working this then needs to be added to the URL.
               img.src = 'https://picsum.photos/id/' + data.id + '/300/300'
+              photographer.insertAdjacentHTML('beforeend', data.author) // This remains the same due to the photograph class wanting to grab the different name everytime syncing up with the innerHTML reset.
       })
     })
   });
 });
 
-// fetch(url)
-//   .then(res => res.json())
-//   .then(result => {
-//     console.log(result)
-//     image.src = result.ressage
-//   })
-//   .catch(err=>console.log(err))
-
-// New Promise: Author
-
+// The console log can be commented out however I like having it open to make sure the data array agrees with the photographer tag and the div for my images. 
+// With this event listener there is one bug and thats if you click the button to quickly then it will cause the Photographer tag to show two names.
+// If I have time after doing a lot more of this project an idea to fix this bug would be to set a timeout. This would take a bit more researching so I will leave this for now. 
 
 
 // Email
-// const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-// const email = document.querySelector('#user-email'); 
-// const form = document.querySelector('.form');
 
-// form.addEventListener('submit', (e) =>{
-//   if (email.value === "" || email.value == null) { 
-//       document.getElementById("#user-email").style.borderColor = "red";
-//       alert("Email is required");
-//   } else if (email.value.match(regex)) {
-//       document.getElementById("#user-email").style.borderColor = "green";
-//   } else {
-//       document.getElementById("#user-email").style.borderColor = "red";
-//       alert("This is an invalid Email Address! Please enter another or check it over.")
-//   }
-//   });
-  
-// 
+const regex = /(^(?!.*__.*)[a-z0-9]{4,253}(_?)[a-z0-9]+(?:\.[a-z0-9!#$%&*+\/=?^`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9]*[a-z0-9])?$)/;
+const email = document.querySelector('#email'); 
+const submitBtn = document.getElementById('#submit-btn')
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (email.value === "" || email.value == "null") {
+    document.getElementById('email').style.borderColor="red";
+    document.getElementById('hidden-warning').style.display="block";
+    document.getElementById('hidden-correct').style.display="none";
+  } else if (email.value.match(regex)) {
+    document.getElementById('email').style.borderColor="green";
+    document.getElementById('hidden-warning').style.display="none";
+    document.getElementById('hidden-correct').style.display="inline";
+  } else {
+    document.getElementById('email').style.borderColor="red";
+    document.getElementById('hidden-warning').style.display="block";
+    document.getElementById('hidden-correct').style.display="none";
+  }
+})
+
 
 
 
