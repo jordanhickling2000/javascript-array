e0e038656ae04c1889b5e3045ab1cc08a479365d
Jordan Hickling <jordan.hickling@netmatters-scs.com>
2023-02-20 17:04:22 +0000

    updated

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let img = new Image(300, 300); //creates a new image at the size of 300x300
 changeImage.appendChild(img); //This should append the .images div to the new image that is created. The images class is the parent and its appending the child to the parent. 
 const photographer = document.querySelector('.photographer');
 const pictureBtn = document.querySelector('#picture-btn');
+const id = img.src;
 
 // Fetches the variable URL: Picsum photos link
 fetch(url)
@@ -54,44 +55,44 @@ const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const email = document.querySelector('#email'); 
 const submitBtn = document.getElementById('#submit-btn')
 const form = document.querySelector('.form');
+const existingImg = document.getElementById('array-image');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (email.value === "" || email.value == "null") {
-    document.getElementById('email').style.borderColor="red";
+    document.getElementById('email').style.borderColor="#981616";
     document.getElementById('hidden-warning').style.display="block";
     document.getElementById('hidden-correct').style.display="none";
   } else if (email.value.match(regex)) {
     document.getElementById('email').style.borderColor="green";
     document.getElementById('hidden-warning').style.display="none";
     document.getElementById('hidden-correct').style.display="inline";
+    document.getElementById('email').style.paddingRight = "30px";
     document.getElementById('empty-text').style.display="none";
     // document.getElementById('image-and-email').style.display="inline"; - Works but doesnt do over 1
-    img.src = ''
-  photographer.innerHTML = 'Photographer: '; // Resets the photographer div everytime a click is heard. Inserting the starting text "Photographer"
+  //   img.src = ''
+  // photographer.innerHTML = 'Photographer: '; // Resets the photographer div everytime a click is heard. Inserting the starting text "Photographer"
   
-  let newLink = new Promise((resolve, reject) => {
-    fetch(url)
-        .then(response => response.headers.get('picsum-id'))
-        .then(info => 'https://picsum.photos/id/' + info + '/info' )
-        .then(resolve => {
-            fetch(resolve)
-            .then(response => response.json()) // Up to this section everything is the same as the first fetch as the same URL and info is being used. The next line becomes different to get the data array.
-            .then( data => {
-              // console.log(data); //This displays the data in an array showing that the data is working this then needs to be added to the URL.
-              img.src = 'https://picsum.photos/id/' + data.id + '/300/300'
-              photographer.insertAdjacentHTML('beforeend', data.author) // This remains the same due to the photograph class wanting to grab the different name everytime syncing up with the innerHTML reset.
-      })
-    })
-  });
+  // let newLink = new Promise((resolve, reject) => {
+  //   fetch(url)
+  //       .then(response => response.headers.get('picsum-id'))
+  //       .then(info => 'https://picsum.photos/id/' + info + '/info' )
+  //       .then(resolve => {
+  //           fetch(resolve)
+  //           .then(response => response.json()) // Up to this section everything is the same as the first fetch as the same URL and info is being used. The next line becomes different to get the data array.
+  //           .then( data => {
+  //             // console.log(data); //This displays the data in an array showing that the data is working this then needs to be added to the URL.
+  //             img.src = 'https://picsum.photos/id/' + data.id + '/300/300'
+  //             photographer.insertAdjacentHTML('beforeend', data.author) // This remains the same due to the photograph class wanting to grab the different name everytime syncing up with the innerHTML reset.
+    //   })
+    // })
+  // });
   } else {
-    document.getElementById('email').style.borderColor="red";
+    document.getElementById('email').style.borderColor="#981616";
     document.getElementById('hidden-warning').style.display="block";
     document.getElementById('hidden-correct').style.display="none";
   }
 })
-
-const id = img.src;
 
 let emailArr = [];
 let imageArr = [];
@@ -106,6 +107,10 @@ function pushData() {
         emailArr[i].push([img.src]);
         container.children[i].insertAdjacentHTML('beforeend',
         `<img id="array-image" src="` + emailArr[i][emailArr[i].length -1] + `">`);
+        console.log(emailArr[i][emailArr[i].length -1])
+        break;
+      } else if(emailArr[i][emailArr[i].length -1] === emailArr[i][emailArr[i].length -1]){
+        console.log('found')
         break;
       } else if (email.value !== emailArr[i][0] && i === emailArr.length - 1) {
         emailArr.push([email.value, img.src]);
@@ -116,7 +121,7 @@ function pushData() {
         `<div id="image-and-email">
         </div>`);
         break;
-      }
+      } 
     }
     if(!Array.isArray(emailArr) || !emailArr.length) {
       emailArr.push([email.value, img.src]);
@@ -130,7 +135,7 @@ function pushData() {
   }
 }
 
-
+// (emailArr[i][0+1])
 
 
 
